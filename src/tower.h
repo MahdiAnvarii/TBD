27e4e5686a97bcb5dc7e9f671edc29ae99672f8e
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2025-05-13 14:31:23 +0330

    Enhance Tower class with advanced combat mechanics
    - Upgraded tower combat system:
      * Smart pointer integration for Balloons
      * Bomb tower splash damage (80px radius)
      * Area-of-effect calculations
      * Sound effects system
    
    - New combat features:
      * countNearbyBaloons() for blast radius checks
      * killNearbyBaloons() for AOE damage
      * SFML audio integration
      * Configurable blast radius
    
    - Technical improvements:
      * Modernized to shared_ptr Balloon handling
      * Added SoundBuffer/Sound for effects
      * Const-correct method signatures
      * Better memory management
    
    - Gameplay impact:
      * Distinct tower behaviors
      * Bomb tower splash damage
      * Audio feedback
      * Strategic depth

--- src/tower.h
+++ src/tower.h
@@ -9,7 +9,9 @@ public:
     Tower(int row_, int col_, int towerPrice_, TowerType towerType_);
     void setTowerType();
     void render(RenderWindow& window);
-    void tryShoot(vector<Baloon>& baloons);
+    void tryShoot(vector<shared_ptr<Baloon>>& baloons);
+    int countNearbyBaloons(const shared_ptr<Baloon>& target, const vector<shared_ptr<Baloon>>& baloons);
+    void killNearbyBaloons(shared_ptr<Baloon>& target, vector<shared_ptr<Baloon>>& baloons);
 private:
     int row;
     int col;
@@ -23,6 +25,9 @@ private:
     CircleShape boundCircle;
     Clock shootCooldown;
     float cooldownTime;
+    float bombBlastRadius = 80.0f;
+    SoundBuffer shotBuffer;
+    Sound shotSound;
 };
 
 #endif //TOWER_H
